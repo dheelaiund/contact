650aaa6b51e4539d3c632bcdccdab604850b6d88
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2022-01-07 23:04:38 +0530

    feat: updated urls

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import re_path,include
+from . import views
+
+
+app_name = 'accounts'
+
+urlpatterns = [
+    re_path(r'^home/$',views.Home.as_view(), name = 'home'),
+    re_path(r'^register/$',views.RegisterUser.as_view(), name = 'registeruser'),
+    re_path(r'^login/$',views.LoginUser.as_view(), name = 'loginuser'),
+    re_path(r'^logout/$',views.LogoutUser.as_view(), name = 'logoutuser'),
+]
