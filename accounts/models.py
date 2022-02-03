6a857bb22503460b3742a84831fc86441cdab684
KKhushhalR2405 <reddykhushhal2405@gmail.com>
2022-02-04 00:28:27 +0530

    added user auth

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 from django.contrib import auth
+from django.urls import reverse
 # Create your models here.
 
 
 class User(auth.models.User, auth.models.PermissionsMixin):
+    
+
     def __str__(self):
         return self.username
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse(
+            "contact:displaycontacts",
+            kwargs={
+                "username": self.user.username,
+                "pk": self.pk
+            }
+        )
+    
+
+
+    
